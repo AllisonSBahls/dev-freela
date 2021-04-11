907c245d5beaafd961b368139c3b7c45ca5b7c83
AllisonSBahls <allison_sousa_bahls@hotmail.com>
2021-04-10 22:34:01 -0400

    Services and DTOs Entitie Skill

---
+++ DevFreela/DevFreela.Application/Services/Implementations/SkillService.cs
@@ -0,0 +1,29 @@
+﻿using DevFreela.Application.Services.Interfaces;
+using DevFreela.Application.ViewModel;
+using DevFreela.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DevFreela.Application.Services.Implementations
+{
+    public class SkillService : ISkillService
+    {
+        private readonly DevFreelaDbContext _context;
+
+        public SkillService(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+        public List<SkillViewModel> GetAll()
+        {
+            var skill = _context.Skills;
+
+            var skillViewModel = skill
+                .Select(p => new SkillViewModel(p.Id, p.Description)).ToList();
+
+            return skillViewModel;
+        }
+    }
+}
---
+++ DevFreela/DevFreela.Application/ViewModel/SkillViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFreela.Application.ViewModel
+{
+    public class SkillViewModel
+    {
+        public int Id { get; private set; }
+        public string Description { get; private set; }
+
+        public SkillViewModel(int id, string description)
+        {
+            Id = id;
+            Description = description;
+        }
+    }
+}
