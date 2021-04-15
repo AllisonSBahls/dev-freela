23d1dd9a039bd828d6bfb5b0235f2fad79167b50
AllisonSBahls <allison_sousa_bahls@hotmail.com>
2021-04-15 14:06:28 -0400

    Generate JWT

--- DevFreela/DevFreela.Core/Entities/User.cs
+++ DevFreela/DevFreela.Core/Entities/User.cs
@@ -12,19 +12,22 @@ namespace DevFreela.Core.Entities
         public DateTime BirthDate { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public bool Active { get; set; }
-
+        public string Password { get; private set; }
+        public string Role { get; private set; }
 
         public List<UserSkill> Skills { get; private set; }
         public List<Project> OwnedProjects { get; private set; }
         public List<Project> FreelanceProjects { get; set; }
         public List<ProjectComment> Comments { get; private set; }
 
-        public User(string name, string email, DateTime birthDate)
+        public User(string name, string email, DateTime birthDate, string password, string role)
         {
             Name = name;
             Email = email;
             BirthDate = birthDate;
             Active = true;
+            Password = password;
+            Role = role;
 
             CreatedAt = DateTime.Now;
             Skills = new List<UserSkill>();
